fd164f154b0c3944524c17bc6df1b859a77b5821
LochNessa131 <128571577+LochNessa131@users.noreply.github.com>
2023-10-29 01:30:13 -0400

    Modify code for better functionality and make prompt case insensitive

--- javascript.js
+++ javascript.js
@@ -1,14 +1,42 @@
+//let string = ("RocK");
+
+let string = prompt("Choose your weapon", "")
+
+let playerSelection = string.toLowerCase();
+let computerSelection = getComputerChoice();
+
 function getComputerChoice () {
-
   let randomOutcome = Math.floor(Math.random() * 3);
-
   if (randomOutcome === 0) {
-    console.log("Rock");
+    return "rock";
 
   } else if (randomOutcome === 1) {
-    console.log("Paper");
+    return "paper";
 
   } else if (randomOutcome === 2) {
-    console.log("Scissors");
+    return "scissors";
   }
 }
+
+function letRounds (playerSelection, computerSelection) {
+if (playerSelection == "rock" && computerSelection === "rock" ||
+    playerSelection == "paper" && computerSelection === "paper" ||
+    playerSelection == "scissors" && computerSelection === "scissors") {
+  return ("It's a tie!");
+    } else if (playerSelection == "rock" && computerSelection === "paper") {
+    return ("You Lose! Paper beats Rock");
+    } else if (playerSelection == "rock" && computerSelection === "scissors") {
+      return ("You Win! Rock beats Scissors");
+    } else if (playerSelection == "paper" && computerSelection === "rock") {
+      return ("You Win! Paper beats Rock");
+    } else if (playerSelection == "paper" && computerSelection === "scissors") {
+      return ("You Lose! Scissors beat Paper");
+    } else if (playerSelection == "scissors" && computerSelection === "rock") {
+      return ("You Lose! Rock beats Scissors");
+    } else if (playerSelection == "scissors" && computerSelection === "paper") {
+      return ("You Win! Scissors beat Paper");
+  } else {
+    console.log("Did not work, try again");
+ } 
+}
+console.log(letRounds(playerSelection, computerSelection));
